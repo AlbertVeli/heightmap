365d5220a083f76e54decc82b85175ac847bdeef
Albert Veli <albert.veli@gmail.com>
2013-01-12 15:20:25 +0100

    Offset merged elevation data with lowest point.
    First search both elevation datafiles for lowest point.
    Then use lowest point as 0 meters in merged file instead
    of sea level as 0 meters.

--- merge_topo_bathy.c
+++ merge_topo_bathy.c
@@ -18,32 +18,40 @@
 #define BATHYFILE "datafiles/gebco_bathy.21601x10801.bin"
 #define OUTFILE "datafiles/merged_topo_bathy.86400x43200.bin"
 
-#if 0
+int16_t min = 0;
+int16_t max = 0;
+
 void find_extremes(char *mp, int w, int h)
 {
    int x, y;
    int16_t *p = (int16_t *)mp;
-   int16_t elevation, min, max;
+   int16_t elevation, lmin, lmax;
 
-   min = 0;
-   max = 0;
+   lmin = 0;
+   lmax = 0;
 
    for (y = 0; y < h; y++) {
       for (x = 0; x < w; x++) {
          elevation = ntohs(*p++);
-         if (elevation < min) {
-            min = elevation;
+         if (elevation < lmin) {
+            lmin = elevation;
          }
-         if (elevation > max) {
-            max = elevation;
+         if (elevation > lmax) {
+            lmax = elevation;
          }
       }
    }
 
-   printf(" - min: %d\n", min);
-   printf(" - max: %d\n", max);
+   printf(" - min: %d\n", lmin);
+   printf(" - max: %d\n", lmax);
+
+   if (lmin < min) {
+      min = lmin;
+   }
+   if (lmax > max) {
+      max = lmax;
+   }
 }
-#endif
 
 void merge_topo_bathy(void)
 {
@@ -52,11 +60,34 @@ void merge_topo_bathy(void)
    int16_t *sp, *bp;
    int16_t s_elevation, b_elevation, elevation;
    int count, county;
+   int16_t offset;
 
    if (!(fp = fopen(OUTFILE, "w"))) {
       perror(OUTFILE);
       return;
    }
+
+   /* Move every point up offset meters so lowest point of ocean floor
+    * becomes 0. If offset is 10577 then ocean level becomes 10577m and
+    * greatest elevation (8579m above sea) becomes 10577 + 8579 = 19156m.
+    * This is within the limits of a signed 16 bit integer, which can
+    * hold values up to 2^15 = 32768m.
+    *
+    * Set offset to 0 if you want to keep sea level at 0m.
+    */
+   if (min < 0) {
+      /* Set offset to lowest point found by find_extremes */
+      offset = abs(min);
+   } else {
+      /* default, 10577 = lowest point in gebco_bathy.21601x10801.bin
+       * use that as offset if find_extremes was skipped.
+       *
+       * Change this to 0 to keep sea level at 0m.
+       */
+      offset = 10577;
+   }
+
+   printf("Merging files to %s, using offset %d meters\n", OUTFILE, offset);
 
    sp = (int16_t *)map[0];
    bp = (int16_t *)map[1];
@@ -78,6 +109,7 @@ void merge_topo_bathy(void)
          }
 
          /* Write elevation (2 bytes) to destination file */
+         elevation += offset;
          fwrite(&elevation, 2, 1, fp);
 
          /* Step up bp every fourth pixel (see dimensions) */
@@ -120,7 +152,8 @@ int main(void)
       return 1;
    }
 
-#if 0
+   /* Change to #if 1 to search both bin-files for lowest point */
+#if 1
    printf("%s:\n", SRTMFILE);
    find_extremes(map[0], S_MAPW, S_MAPH);
    /* datafiles/srtm_ramp2.world.86400x43200.bin:
@@ -138,6 +171,9 @@ int main(void)
    merge_topo_bathy();
 
    free_maps();
+
+   puts("Done. To use the merged height data, use option -h to hm:\n");
+   printf("./hm -h %s 74.0 3.0 19.0 27.0 scandinavia\n\n", OUTFILE);
 
    return 0;
 }
